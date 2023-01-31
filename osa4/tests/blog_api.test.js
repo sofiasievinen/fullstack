e70ae81c41917f25cb11e3dacafe6954456b7576
Sofia Sievinen <sofiasievinen@gmail.com>
2023-01-31 15:55:40 +0200

    4.10 done

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -43,6 +43,29 @@ test('id is not called _id', async () => {
     expect(response.body[0].id).toBeDefined
 })
 
+test('a valid blog can be added', async () => {
+    const newBlog =     {
+        title: "kolmas",
+        author: "tyyppi 3",
+        url: "www.lisaatesteja.fi",
+        likes: 914
+      }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response = await api.get('/api/blogs')
+  
+    const titles = response.body.map(r => r.title)
+    expect(response.body).toHaveLength(initialBlogs.length +1)
+    expect(titles).toContain(
+      'kolmas'
+    )
+  })
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
