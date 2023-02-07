e1422fd38cd68c8c4dc4d2761eef1fe7e9cf52c0
Sofia Sievinen <sofiasievinen@gmail.com>
2023-02-07 21:10:40 +0200

    4.20 done

--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -26,8 +26,18 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    const token = authorization.replace('Bearer ', '')
+    request.token = token
+  }
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler, 
+  tokenExtractor
 }
