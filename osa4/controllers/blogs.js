ddd7690626a4ee70fd7b5d5b4f73f8a129d09ff4
Sofia Sievinen <sofiasievinen@gmail.com>
2023-02-07 16:29:17 +0200

    4.17 done

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+  .find({}).populate('user', { username: 1, name: 1, id: 1})
   response.json(blogs)
 })
 
@@ -10,6 +13,8 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   
   const body = request.body
+  const user = await User.findById(body.userId)
+
 
   if (body.title && body.url) {
 
@@ -23,9 +28,12 @@ blogsRouter.post('/', async (request, response) => {
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: 0
+        likes: 0,
+        user: user._id
       })
       const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
       response.status(201).json(savedBlog)
     }
   }
